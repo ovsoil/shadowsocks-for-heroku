ee2968fc58663bb93f998e89fbf932a3790014b5
ovsoil <hxyumail@126.com>
2019-10-30 14:54:00 +0800

    clone for gmail heroku account

---
+++ start_euss.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+node local.js -s eu-g-ss.herokuapp.com -l 1090 -m aes-256-cfb -k huaxin -r 80
--- start_proxy.sh
+++ start_proxy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node local.js -s shadowsocks-for-heroku.herokuapp.com -l 1080 -m aes-256-cfb -k huaxin -r 80
+node local.js -s g-ss.herokuapp.com/ -l 1090 -m aes-256-cfb -k huaxin -r 80
